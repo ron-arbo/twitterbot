427aea4cbb207e73e2fc9616db81c417aa3efbb1
ron_arbo <ron.arbo63@icloud.com>
2020-01-16 23:21:07 -0500

    Push initial files

---
+++ bot.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from time import sleep
+
+class TwitterBot:
+    def __init__(self, username, password):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://twitter.com")
+        sleep(2)
+
+        logInxPath = "/html/body/div[1]/div/div[1]/div[1]/div[2]/div[2]/div/a[2]"
+        logIn = self.driver.find_element_by_xpath(logInxPath)
+        logIn.click()
+
+        sleep(2)
+        
+        uNamexPath = "/html/body/div[1]/div[2]/div/div/div[1]/form/fieldset/div[1]/input"
+        uName = self.driver.find_element_by_xpath(uNamexPath)
+        uName.send_keys(username)
+
+        sleep(2)
+
+        pWordxPath = "/html/body/div[1]/div[2]/div/div/div[1]/form/fieldset/div[2]/input"
+        pWord = self.driver.find_element_by_xpath(pWordxPath)
+        pWord.send_keys(password)
+
+        sleep(2)
+
+        logxPath = "/html/body/div[1]/div[2]/div/div/div[1]/form/div[2]/button"
+        log = self.driver.find_element_by_xpath(logxPath)
+        log.click()
+
+        sleep(5)
+
+    def retweet(self):
+        driver = self.driver
+
+        #Scroll to load tweets
+        for i in range(1, 5):
+            driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+            sleep(2)
+            
+
+        
+
+bot = TwitterBot("funkyduckling39", "the_DUCK22")
+bot.retweet()
